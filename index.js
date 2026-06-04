59479dc68be2c55b260796e725cd780afad8eebd
Thanzia Patel Raheem <thanzia.pr@gmail.com>
2026-06-04 16:20:04 +0800

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from server! Backend is running");
+  res.send("Hello from server! Backend is running";
 });
 
 app.listen(PORT, "0.0.0.0", () => {
